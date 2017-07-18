220bdca3969118aa869960333753ba6269dc6049
Pawel Psztyc <jarrodek@gmail.com>
2017-07-18 01:07:23 +0100

    New: Added more test cases related to framgments.

--- test/test.js
+++ test/test.js
@@ -1,7 +1,10 @@
 const {RamlJsonGenerator} = require('..');
 const fs = require('fs');
 
-const apiUrl = 'https://cdn.rawgit.com/advanced-rest-client/raml-example-api/master/api.raml';
+// const apiUrl = 'test/address-1.0.0-raml-fragment/address.raml';
+// const apiUrl = 'test/employment-1.0.0-raml-fragment/employment.raml';
+// const apiUrl = 'test/phone-1.0.0-raml-fragment/phone.raml';
+const apiUrl = 'test/drive-raml-api-v2/api.raml';
 const enhancer = new RamlJsonGenerator(apiUrl);
 enhancer.generate()
 .then((data) => {
